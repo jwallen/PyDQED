b31b977a8526b10bfaf754c5e9c19ac703f260e2
Josh Allen <jwallen@mit.edu>
2011-02-05 11:58:56 -0500

    Added/updated build scripts to compile the pydqed Cython module.
    These scripts generally assume that you are using gcc/gfortran as your
    compilers, as these are the supported compilers.

--- Makefile
+++ Makefile
@@ -8,7 +8,13 @@ F90=gfortran
 
 CFLAGS=-fPIC -O3
 
-all: DQED
+all: DQED cython
+
+cython:
+	python setup.py build_ext --build-lib . --build-temp build --pyrex-c-in-temp
+
+install:
+	python setup.py install
 
 DQED: libdqed.a
 
@@ -19,8 +25,32 @@ build/dqed.o: dqed.f90
 	mkdir -p build
 	$(F90) $(CFLAGS) -c dqed.f90 -o build/dqed.o
 
-clean: clean-DQED
+clean: clean-DQED clean-cython
 	rm -rf build
 
 clean-DQED:
 	rm build/dqed.o libdqed.a
+
+clean-cython:
+	python setup.py clean --build-temp build
+	rm -f *.so *.pyc
+
+help:
+	@echo ""
+	@echo "This makefile can be used to build PyDQED and its dependencies."
+	@echo ""
+	@echo "Typing \`make\` with no arguments will compile DQED to a static library"
+	@echo "and compile the PyDQED Python module that provides the Python interface"
+	@echo "to DQED."
+	@echo ""
+	@echo "Typing \`make clean\` will delete all of the intermediate build files,"
+	@echo "compiled libraries, and compiled Python modules for DQED and the PyDQED"
+	@echo "modules."
+	@echo ""
+	@echo "Individual dependencies can be specified using \`make <target>\`, where"
+	@echo "<target> is one of:"
+	@echo ""
+	@echo "    DQED     to compile the DQED code"
+	@echo "    cython   to compile the PyDQED Python wrapper modules"
+	@echo ""
+
---
+++ setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+################################################################################
+#
+#   PyDQED - A Python wrapper for the DQED constrained nonlinear optimization code
+#
+#   Copyright (c) 2011 by Joshua W. Allen (jwallen@mit.edu)
+#
+#   Permission is hereby granted, free of charge, to any person obtaining a
+#   copy of this software and associated documentation files (the 'Software'),
+#   to deal in the Software without restriction, including without limitation
+#   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+#   and/or sell copies of the Software, and to permit persons to whom the
+#   Software is furnished to do so, subject to the following conditions:
+#
+#   The above copyright notice and this permission notice shall be included in
+#   all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#   DEALINGS IN THE SOFTWARE.
+#
+################################################################################
+
+import numpy
+
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+# Turn on HTML annotation file generation
+import Cython.Compiler.Options
+Cython.Compiler.Options.annotate = True
+
+# The Cython extension modules to compile
+ext_modules = [
+    Extension(
+        'pydqed', 
+        ['pydqed.pyx'], 
+        include_dirs=['.', numpy.get_include()], 
+        libraries=['gfortran'], 
+        extra_objects=['build/dqed.o'],
+    ),
+]
+
+# Run the setup command
+setup(name='PyDQED',
+    version='0.1.0',
+    description='A Python wrapper to the DQED bounded constrained nonlinear optimization code',
+    author='Joshua W. Allen',
+    author_email='jwallen@mit.edu',
+    url='http://github.com/jwallen/PyDQED',
+    py_modules=['pydqed'],
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = ext_modules
+)
